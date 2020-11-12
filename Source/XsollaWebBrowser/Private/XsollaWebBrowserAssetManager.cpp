80f51467f743a2fb3ee5a18ae70d2a1c2e8982e9
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2020-11-12 14:25:27 +0200

    SDK-836 Fix build issues

--- Source/XsollaWebBrowser/Private/XsollaWebBrowserAssetManager.cpp
+++ Source/XsollaWebBrowser/Private/XsollaWebBrowserAssetManager.cpp
@@ -10,7 +10,7 @@
 UXsollaWebBrowserAssetManager::UXsollaWebBrowserAssetManager(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 	, DefaultMaterial(FString(TEXT("/XsollaInventory/Common/Browser/M_WebTexture.M_WebTexture")))
-	, DefaultTranslucentMaterial(FString(TEXT("/XsollatoryInventory/Common/Browser/M_WebTexture_Translucent.M_WebTexture_Translucent")))
+	, DefaultTranslucentMaterial(FString(TEXT("/XsollaInventory/Common/Browser/M_WebTexture_Translucent.M_WebTexture_Translucent")))
 {
 #if WITH_EDITOR || PLATFORM_ANDROID || PLATFORM_IOS
 	// Add a hard reference to UXsollaWebBrowserTexture, without this the WebBrowserTexture DLL never gets loaded on Windows.
